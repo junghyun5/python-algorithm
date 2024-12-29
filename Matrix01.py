e38f68f7fc602496fdb031be673a97498d7983f4
daummm <50vjtpsxm@naver.com>
2024-12-29 15:57:52 -0600

    Create Matrix01.py

---
+++ Matrix01.py
@@ -0,0 +1,16 @@
+N,M = map(int,input().split) #matrix size
+A = [] #make array
+B = []
+for i in range(N): #input the array 
+    A.append([])
+    A[i] = list(map(int,input().split()))
+
+for i in range(N):
+    B.append([])
+    B[i] = list(map(int,input().split()))
+
+for i in range(N): #Matrix Sum
+    for j in range(M):
+        print(A[i][j]+B[i][j], end=' ')
+    print()
+
