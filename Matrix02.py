f1576508aae2a3e513295a6deab0dd7b91c4b904
daummm <50vjtpsxm@naver.com>
2024-12-30 21:50:02 -0600

    Create Matrix02.py

---
+++ Matrix02.py
@@ -0,0 +1,19 @@
+#BackJoon Algorithm NO.2566
+#ver.01
+N = []
+A = -1 #min 0 max99
+B,C= 0,0
+for i in range(9):
+    N.append([]) #2D array
+    #or N = [list(map(int,input().split())) for _ in range(9)]
+    N[i] = list(map(int,input().split()))
+    for j in range(9): 
+        if N[i][j] > A:
+            A = N[i][j]
+            B = i + 1
+            C = j + 1
+
+print(A)
+print(B, C)
+
+            
