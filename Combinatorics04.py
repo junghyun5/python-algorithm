118b1bc685e50d5b62c3cabb136c5aa48e0d7f6d
daummm <50vjtpsxm@naver.com>
2025-01-08 10:36:59 -0600

    Create Combinatorics04.py

---
+++ Combinatorics04.py
@@ -0,0 +1,17 @@
+#BackJoon Algorithm NO.11050
+#Combinatorics/binomial coefficient
+def factorial(N):
+    if N == 0: #Base 
+        return 1
+    return factorial(N-1) * N #Recursive
+
+def main(): #input range N:1~10, K:0~N
+    N,K= map(int,input().split())
+    result = factorial(N)//(factorial(K)*factorial(N-K)) #Used integer division
+    print(result)
+    
+    
+if __name__ == '__main__':
+    main()
+        
+        
