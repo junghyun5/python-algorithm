8250f12f8a5dd1371bd7ea9458c532e4da81b820
daummm <50vjtpsxm@naver.com>
2025-01-08 10:46:58 -0600

    Update Combinatorics04.py

--- Combinatorics04.py
+++ Combinatorics04.py
@@ -9,6 +9,15 @@ def main():#input range N:1~10, K:0~N
     N,K= map(int,input().split())
     result = factorial(N)//(factorial(K)*factorial(N-K)) #Used integer division
     print(result)
+    #Different solution
+    # result = 1
+    # for i in range(K):
+    #     result *= N
+    #     N -= 1
+    # divs = 1
+    # for i in range(2,K+1):
+    #     divs *= 1
+    #print(result // divs)
     
 if __name__ == '__main__':
     main()
