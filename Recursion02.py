9ea933d2f37fcfe8bd431ba6d46079bc0a450e95
daummm <50vjtpsxm@naver.com>
2025-01-21 07:25:57 -0600

    Create Recursion02.py

---
+++ Recursion02.py
@@ -0,0 +1,20 @@
+#BackJoon Algorithm NO.27433
+#Recursion
+import sys
+input = sys.stdin.readline
+
+def Fibonacci(N):
+    if N == 0:
+        return 0
+    elif N == 1:
+        return 1
+    else:
+        return Fibonacci(N-2) + Fibonacci(N-1)
+    
+def main():
+    N = int(input())
+    Fi = Fibonacci(N)
+    print(Fi)
+    
+if __name__ == '__main__':
+    main()
