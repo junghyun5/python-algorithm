f54b3bde93708d438d8e5fa29d1ebe3f8b61f38a
daummm <50vjtpsxm@naver.com>
2025-01-20 22:32:52 -0600

    Create Recursion01.py

---
+++ Recursion01.py
@@ -0,0 +1,18 @@
+#BackJoon Algorithm NO.27433
+#Recursion
+import sys
+from collections import Counter
+input = sys.stdin.readline
+
+def factorial(N):
+    if N <= 1:
+        return 1
+    return factorial(N-1)*N
+
+def main():
+    N = int(input())
+    Fac = factorial(N)
+    print(Fac)
+
+if __name__ == '__main__':
+    main()
