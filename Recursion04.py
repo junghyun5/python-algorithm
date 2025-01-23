cc27626162a7f04904a0dddfe34bf11e9aef37a8
daummm <50vjtpsxm@naver.com>
2025-01-23 17:42:36 -0600

    Update Recursion04.py

--- Recursion04.py
+++ Recursion04.py
@@ -1,2 +1,17 @@
 #BackJoon Algorithm NO.24060
 #Recursion
+
+def merge(arr1):
+    sorted_a = []
+    while ()
+    
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    mid = len(arr) // 2
+    arr1 = merge_sort(arr[:mid])
+    arr2 = merge_sort(arr[mid:])
+    merge(arr1, arr2)
+    
+
+    
