6612cf1d9b91f44aec55cfd7ec5e463198c63166
daummm <50vjtpsxm@naver.com>
2025-01-22 21:13:34 -0600

    Create NumberTheory05.py

---
+++ NumberTheory05.py
@@ -0,0 +1,25 @@
+#BackJoon Algorithm NO.4134
+#Number theory
+
+import sys
+import math
+input = sys.stdin.readline
+
+def sos(x):
+    for i in range(2, int(math.sqrt(x)+1)):
+        if x%i == 0:
+            return False
+    return True
+    
+N = int(input())
+for i in range(N):
+    n = int(input())
+    while True:
+        if n==0 or n == 1:
+            print(2) #0,1은 미리 제외해버리네
+            break
+        elif sos(n):
+            print(n)
+            break
+        else:
+            n += 1
