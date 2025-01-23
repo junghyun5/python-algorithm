063a0dfe909c5095457668c639d6db188970f1c2
daummm <50vjtpsxm@naver.com>
2025-01-22 21:12:07 -0600

    Update NumberTheory04.py

--- NumberTheory04.py
+++ NumberTheory04.py
@@ -11,22 +11,22 @@ def gcd(x,y):
     return x
 
 n = int(input())
-cnt = 0 # 숫자 세기 위한 변수 선언
+cnt = 0 # Declare a variable to count the numbers
  
 arr = sorted([int(input()) for _ in range(n)])
-#list 만들고 정렬. 컴프리헨션 
+#Create a list and sort it using list comprehension
     
 sub = []
 
 for i in range(len(arr) -1):
     sub.append(arr[i+1] - arr[i])
-# 가로수들의 차이를 리스트에 저장
+#  Store the differences between the trees in a list
 
 gcd1 = sub[0]
 for i in range(1,len(sub)):
-    gcd1 = gcd(gcd1,sub[i]) #각각의 최대공약수 구하기 
+    gcd1 = gcd(gcd1,sub[i]) #Calculate the greatest common divisor for each of the differences 
 
 for j in sub:
-    cnt +=  j // gcd1 -1 #-1은 왜 해주냐면 그 사이에 심는 거잖아.
+    cnt +=  j // gcd1 -1 #The -1 is because we're planting trees in between the existing ones.
     
 print(cnt)
