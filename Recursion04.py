8113b47481735fad0126bbc6d1b828c3f2217226
daummm <50vjtpsxm@naver.com>
2025-01-23 23:39:34 -0600

    Update Recursion04.py

--- Recursion04.py
+++ Recursion04.py
@@ -1,7 +1,10 @@
 #BackJoon Algorithm NO.24060
 #Recursion
-count = []
 
+count = []
+# This list will track the order of elements while sorting.
+
+# Merge function to combine two sorted arrays into one sorted array
 def merge(arr1, arr2):
     global count
     sorted_a = []
@@ -27,7 +30,8 @@ def merge(arr1, arr2):
         h += 1
         
     return sorted_a
-    
+
+# Merge Sort function that splits the array and recursively sorts it   
 def merge_sort(arr):
     if len(arr) <= 1:
         return arr
@@ -39,17 +43,16 @@ def merge_sort(arr):
     
     
 def main():
-    N, k = map(int,input().split())
+    N, k = map(int,input().split()) # Read N (size of array) and k (k-th element to find
     # for _ in range(N):
     #     num = int(input())
     #     arr.append(num)
     arr = list(map(int,input().split()))
-    merge_sort(arr)
-    print(count)
-    if len(count) < k:
+    merge_sort(arr) 
+    if len(count) < k: # If there are fewer than k elements in the count list
         print(-1)
     else:
-        print(count[k-1])
+        print(count[k-1]) # Print the k-th element from the count list
 
 if __name__ == '__main__':
     main()
