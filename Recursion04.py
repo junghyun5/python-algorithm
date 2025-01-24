0c97e3d1c604aab6c7c033978c1942050dd8efbb
daummm <50vjtpsxm@naver.com>
2025-01-23 22:23:57 -0600

    Update Recursion04.py

--- Recursion04.py
+++ Recursion04.py
@@ -3,8 +3,14 @@
 
 def merge(arr1, arr2):
     sorted_a = []
-    t = 0
-    while (arr1[])
+    l,h = 0,0
+    while l<len(arr1) and h < len(arr2) :
+        if arr1[l] <= arr2[h]:
+            sorted_a.append(arr1[l])
+            l += 1
+        else:
+            sorted_a.append(arr2[h])
+            h += 1
     
 def merge_sort(arr):
     if len(arr) <= 1:
