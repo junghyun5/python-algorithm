9851896c86665b4259d135ec60396b70cebd718c
daummm <50vjtpsxm@naver.com>
2025-01-24 20:05:36 -0600

    Update Recursion05.py

--- Recursion05.py
+++ Recursion05.py
@@ -1,2 +1,14 @@
 #BackJoon Algorithm NO.4779
 #Recursion
+
+def CantorianSet(N):
+    set1 = ['-'] * (3**N)
+    
+    
+
+while True:
+    try:
+        N = int(input())
+        CantorianSet(N)
+    except : # EOF 
+        break
