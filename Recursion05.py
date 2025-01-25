fb32152afe9d44bc832226f74ee5d915085b38fd
daummm <50vjtpsxm@naver.com>
2025-01-24 20:07:22 -0600

    Update Recursion05.py

--- Recursion05.py
+++ Recursion05.py
@@ -1,14 +1,16 @@
 #BackJoon Algorithm NO.4779
 #Recursion
 
-def CantorianSet(N):
-    set1 = ['-'] * (3**N)
+def CantorianSet(set1):
+    
+    
     
     
 
 while True:
     try:
         N = int(input())
-        CantorianSet(N)
-    except : # EOF 
+        set1 = ['-'] * (3**N)
+        CantorianSet(set1)
+    except : #EOF: Stops execution when writing stops (end of file)
         break
