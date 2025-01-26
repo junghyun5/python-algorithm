4863f1e2007d2aa12f7df5add6d7e50b216680a9
daummm <50vjtpsxm@naver.com>
2025-01-26 13:48:34 -0600

    Update Recursion06.py

--- Recursion06/Recursion06.py
+++ Recursion06/Recursion06.py
@@ -1,38 +1,47 @@
 #BackJoon Algorithm NO.2447
 #Recursion
 
-def blank(N):
-    arr1 = []
-    for _ in range(N):
-            row = " " * N
-            arr1.append(row)
-    return arr1
+# wrong idea 
+# def blank(N):
+#     arr1 = []
+#     for _ in range(N):
+#             row = " " * N
+#             arr1.append(row)
+#     return arr1
         
 def drwStar(N):
     if N == 1:
-        return ("*")
+        return ["*"]
     
     mid = drwStar(N//3)
     arr = []
     
-    for _ in range (3):
-        arr.extend(mid)
-    arr.append("\n")
-    # Use extend instead of append to add each element of mid directly to arr
+    for i in range (3):
+        for j in range(3):
+            if i == 1 and j == 1:
+                arr.extend([' '* (N//3)]*(N//3))
+            else:
+                arr.extend(mid)
+        
+    # wrong idea 02    
+    # arr.append("\n")
+    # # Use extend instead of append to add each element of mid directly to arr
     
-    arr.extend(mid)
-    arr.extend(blank(N//3))
-    arr.extend(mid)
-    arr.append("\n")
+    # arr.extend(mid)
+    # arr.extend(blank(N//3))
+    # arr.extend(mid)
+    # arr.append("\n")
     
-    for _ in range (3):
-        arr.extend(mid)
+    # for _ in range (3):
+    #     arr.extend(mid)
         
     return arr
 
 def main():
     N = int(input())
-    print("".join(drwStar(N)))
+    star = drwStar(N)
+    # Join all the elements of the 'star' list into one string, inserting a newline between each element.
+    print(star)
     
 if __name__ == '__main__':
     main()
