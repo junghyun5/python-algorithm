23639c15544f1d3c9875020fd9684ce4247394c3
daummm <50vjtpsxm@naver.com>
2025-01-28 14:43:15 -0600

    Update Recursion07.py

--- Recursion07/Recursion07.py
+++ Recursion07/Recursion07.py
@@ -6,14 +6,14 @@ def Hanoi(N , start, end):
         print(start,end)
         return
     
-    Hanoi(N-1, start, 6-start-end)
-    print(start, end)
-    Hanoi(N-1, 6-start-end, end)
+    Hanoi(N-1, start, 6-start-end) #N-1
+    print(start, end) #1
+    Hanoi(N-1, 6-start-end, end) #N-1
     
 
 def main():
-    N = int(input())
-    print(2**N-1)
+    N = int(input()) #f(x) = 2^N -1
+    print(2**N-1) # 2f(N-1)+1
     Hanoi(N,1,3)
     
 if __name__ == '__main__':
