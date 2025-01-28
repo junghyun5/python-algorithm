1a1503907afaafa7ddde795009f23d85504eccd9
daummm <50vjtpsxm@naver.com>
2025-01-28 14:34:21 -0600

    Update Recursion07.py

--- Recursion07/Recursion07.py
+++ Recursion07/Recursion07.py
@@ -1,19 +1,19 @@
 #BackJoon Algorithm NO.11729
 #Recursion
-import sys
-input = sys.stdin.readline
 
 def Hanoi(N , start, end):
     if N == 1:
         print(start,end)
+        return
     
-    Hanoi(N-1, start, 6 - start -end)
+    Hanoi(N-1, start, 6-start-end)
     print(start, end)
-    Hanoi(N-1, 6 -start -end, end)
+    Hanoi(N-1, 6-start-end, end)
     
 
 def main():
     N = int(input())
+    print(2**N-1)
     Hanoi(N,1,3)
     
 if __name__ == '__main__':
