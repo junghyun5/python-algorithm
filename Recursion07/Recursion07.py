abef2440332a941b9f0ee652ef0647ea9a89e888
daummm <50vjtpsxm@naver.com>
2025-01-28 14:14:28 -0600

    Update Recursion07.py

--- Recursion07/Recursion07.py
+++ Recursion07/Recursion07.py
@@ -1,2 +1,13 @@
 #BackJoon Algorithm NO.11729
 #Recursion
+
+def Hanoi(N , start, end):
+    if N == 1:
+        print(start,end)
+    
+    Hanoi(N-1, start, 6 - start -end)
+    print(start, end)
+    Hanoi(N-1, 6 -start -end, end)
+    
+
+    
