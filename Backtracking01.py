85da2dc594cde8336d22ce1948dab2122b5135d7
daummm <50vjtpsxm@naver.com>
2025-01-30 16:18:50 -0600

    Update Backtracking01.py

--- Backtracking01.py
+++ Backtracking01.py
@@ -2,11 +2,15 @@
 #Backtracking 
 
 #Using the DFS
-def DFS():
-    
-    
+def DFS(arr):
+    if len(arr) > M: #make a line until M
+        print(' '.join(map(str,arr)))
+        return
+    for i in range(1,N):
+        
 
 N,M = map(int,input().split())
+arr = []
 
 
 
