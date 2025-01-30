ed94f67c822d9ef873dc234d53299737faae57ae
daummm <50vjtpsxm@naver.com>
2025-01-30 16:22:21 -0600

    Update Backtracking01.py

--- Backtracking01.py
+++ Backtracking01.py
@@ -7,10 +7,15 @@ def DFS(arr):
         print(' '.join(map(str,arr)))
         return
     for i in range(1,N):
+        if visited[i] == True:
+            continue
+        visited[i] = True
+        arr.append(i)
         
 
 N,M = map(int,input().split())
 arr = []
+visited = [False] * N
 
 
 
