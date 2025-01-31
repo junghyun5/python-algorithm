b348ca1b8c9a5e42786ec3baa0206c0d6ed03253
daummm <50vjtpsxm@naver.com>
2025-01-31 16:46:22 -0600

    Update Backtracking02.py

--- Backtracking02/Backtracking02.py
+++ Backtracking02/Backtracking02.py
@@ -1,2 +1,25 @@
 #BackJoon Algorithm NO.15650
 #Backtracking 
+
+#Using the DFS
+def DFS(Num):
+    if len(arr) == M: #make a line until M
+        print(' '.join(map(str,arr)))
+        return
+    #Pay attention to the order 
+    for i in range(Num,N):
+        if visited[i] == True: #prevent the same number
+            continue
+        visited[i] = True #change to the True
+        arr.append(i+1) #Put the number in the array
+        DFS(Num+1) #find the next digit
+        #Don't put the DFS(arr), it doesn't need it. 
+        arr.pop() #start a new line, next i.
+        visited[i] = False #reset
+        
+        
+
+N,M = map(int,input().split())
+arr = []
+visited = [False] * N
+DFS(0)
