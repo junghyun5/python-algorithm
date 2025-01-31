42a7f1a9729ae6edf37da410ac40129789b418c7
daummm <50vjtpsxm@naver.com>
2025-01-30 18:20:29 -0600

    Update Backtracking01.py

--- Backtracking01.py
+++ Backtracking01.py
@@ -2,18 +2,19 @@
 #Backtracking 
 
 #Using the DFS
-def DFS(arr):
-    if len(arr) > M: #make a line until M
+def DFS():
+    if len(arr) == M: #make a line until M
         print(' '.join(map(str,arr)))
         return
     #Pay attention to the order 
-    for i in range(1,N):
+    for i in range(0,N):
         if visited[i] == True: #prevent the same number
             continue
         visited[i] = True #change to the True
-        arr.append(i) #Put the number in the array
+        arr.append(i+1) #Put the number in the array
         DFS() #find the next digit
-        del arr(i) #start a new line, next i.
+        #Don't put the DFS(arr), it doesn't need it. 
+        arr.pop() #start a new line, next i.
         visited[i] = False #reset
         
         
@@ -21,7 +22,7 @@ def DFS(arr):
 N,M = map(int,input().split())
 arr = []
 visited = [False] * N
-
+DFS()
 
 
 
