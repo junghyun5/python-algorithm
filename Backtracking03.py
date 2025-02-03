688efcc25b5fc8bf0aebbe9ad34845c7399a527e
daummm <50vjtpsxm@naver.com>
2025-02-03 14:55:33 -0600

    Update Backtracking03.py

--- Backtracking03.py
+++ Backtracking03.py
@@ -1,2 +1,13 @@
 #BackJoon Algorithm NO.15651
 #Backtracking 
+
+def DFS():
+    if len(arr) == M:
+        print(' '.join(map(str,arr)))
+        return
+    for i in range(N):  
+          
+
+
+N,M = map(int,input().split())
+arr = []
