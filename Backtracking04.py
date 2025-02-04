66f4982143ea9702144c17ca468702fe5907d402
daummm <50vjtpsxm@naver.com>
2025-02-04 15:37:25 -0600

    Update Backtracking04.py

--- Backtracking04.py
+++ Backtracking04.py
@@ -1,2 +1,16 @@
 #BackJoon Algorithm NO.15652
 #Backtracking 
+
+def DFS(start):
+    if len(arr) == M:
+        print(' '.join(map(str,arr)))
+        return
+    for i in range(N): #Same number possible
+        arr.append(i+1)
+        DFS(i)  
+        arr.pop()
+
+
+N,M = map(int,input().split())
+arr = []
+DFS(0)
