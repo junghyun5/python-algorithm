2f0688c9ef267022bb3bfb07950eb0a6ada1ed32
daummm <50vjtpsxm@naver.com>
2025-02-10 19:07:18 -0600

    Update Backtracking05.py

--- Backtracking05/Backtracking05.py
+++ Backtracking05/Backtracking05.py
@@ -2,6 +2,19 @@
 #Backtracking 
 
 #divided into two parts.
-def 
+def queen(i, col):
+    n = len(col) - 1
+    # col = [0,0,0, ... ,0]
+    #first one is 
+    if(check(i,col)):
+        if(i == n):
+            print(col[1: n+1])
+    else:
+        for j in range(i,n+1):
+            col[i+1] = j
+            queen(i+1,col)
+    
+def check(i,col):
+    
 
 N = int(input())
