90db19afeb28975c1eba1c3e4e166189b8490a55
daummm <50vjtpsxm@naver.com>
2025-02-11 21:23:59 -0600

    Update Backtracking05.py

--- Backtracking05/Backtracking05.py
+++ Backtracking05/Backtracking05.py
@@ -3,26 +3,26 @@
 
 #divided into two parts.
 def queen(i, col):
+    global count
     n = len(col) - 1
     # col = [0,0,0, ... ,0]
     #first one is 
     if(check(i,col)):
         if(i == n):
-            print(col[1: n+1])
-    else:
-        for j in range(i,n+1):
-            col[i+1] = j
-            queen(i+1,col)
+            count += 1
+        else:
+            for j in range(i,n+1):
+                col[i+1] = j
+                queen(i+1,col)
     
 def check(i,col):
-    k = 1
-    check1 = True
-    while(k< i and check1):
+    for k in range(i):
         if(col[i] == col[k] or abs(col[i] - col[k]) == (i-k)):
-            check1 = False
-        k += 1
-    return check1
+            return False
+    return True
     
 N = int(input())
+count = 0
 col = [0] * (N+1)
 queen(0,col)
+print(count)
