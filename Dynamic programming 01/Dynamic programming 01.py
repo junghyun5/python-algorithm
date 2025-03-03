787a1f232aaa9164a7e0252caa9504aff2a7a7ab
daummm <50vjtpsxm@naver.com>
2025-03-03 09:16:06 -0600

    Update Dynamic programming 01.py

--- Dynamic programming 01/Dynamic programming 01.py
+++ Dynamic programming 01/Dynamic programming 01.py
@@ -4,20 +4,20 @@
 n = int(input())
 
 f1, f2 = 0, 0 #number of repetitions. 
-def fib(n):
+def fib(n): #recursive function
     global f1 
     
     if n == 1 or n == 2:
-        f1 += 1
+        f1 += 1 #counting
         return 1
     else:
         return fib(n-1) + fib(n-2)
     
 f = [0] * (n+1)
-def fibonacci(n):
+def fibonacci(n): #dynamic programming
     f[1] = 1
     f[2] = 1
-    global f2 #how 
+    global f2 
     for i in range(3, n+1):
         f[i] = f[i-1] + f[i-2]
         f2 += 1
